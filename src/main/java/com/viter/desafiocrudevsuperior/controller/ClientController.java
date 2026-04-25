bc65f7bcc6fe233a6f0a4e9ee222ecaba704a0af
Eduardo Viter <moreiraviter@outlook.com>
2026-04-25 19:51:55 -0300

    teste API

---
+++ src/main/java/com/viter/desafiocrudevsuperior/controller/ClientController.java
@@ -0,0 +1,15 @@
+package com.viter.desafiocrudevsuperior.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/client")
+public class ClientController {
+
+    @GetMapping
+    public String test() {
+        return "Hello World!";
+    }
+}
