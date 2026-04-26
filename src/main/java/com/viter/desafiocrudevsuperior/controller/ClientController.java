552ec3a1d3919f4d16acd0aa19f8f696a6c19847
Eduardo Viter <moreiraviter@outlook.com>
2026-04-26 10:14:46 -0300

    busca paginada de produtos

--- src/main/java/com/viter/desafiocrudevsuperior/controller/ClientController.java
+++ src/main/java/com/viter/desafiocrudevsuperior/controller/ClientController.java
@@ -1,16 +1,16 @@
 package com.viter.desafiocrudevsuperior.controller;
 
 import com.viter.desafiocrudevsuperior.dto.ClientDTO;
-import com.viter.desafiocrudevsuperior.entite.Client;
-import com.viter.desafiocrudevsuperior.repositorie.ClientRepository;
 import com.viter.desafiocrudevsuperior.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/client")
@@ -22,5 +22,10 @@ public class ClientController {
     @GetMapping(value = "/{id}")
     public ClientDTO findById(@PathVariable Long id) {
         return service.findById(id);
+    }
+
+    @GetMapping
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        return service.findAll(pageable);
     }
 }
--- src/main/java/com/viter/desafiocrudevsuperior/service/ClientService.java
+++ src/main/java/com/viter/desafiocrudevsuperior/service/ClientService.java
@@ -4,9 +4,12 @@ import com.viter.desafiocrudevsuperior.dto.ClientDTO;
 import com.viter.desafiocrudevsuperior.entite.Client;
 import com.viter.desafiocrudevsuperior.repositorie.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -21,5 +24,11 @@ public class ClientService {
         Client client = result.get();
         ClientDTO dto = new ClientDTO(client);
         return dto;
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable){
+        Page<Client> resutl = repository.findAll(pageable);
+        return resutl.map(x -> new ClientDTO(x));
     }
 }
