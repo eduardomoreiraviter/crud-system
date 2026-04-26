cfdc0ec2cb089c511a37ac67ae48b6ba77444f40
Eduardo Viter <moreiraviter@outlook.com>
2026-04-26 09:13:48 -0300

    teste repository

--- src/main/java/com/viter/desafiocrudevsuperior/controller/ClientController.java
+++ src/main/java/com/viter/desafiocrudevsuperior/controller/ClientController.java
@@ -1,15 +1,25 @@
 package com.viter.desafiocrudevsuperior.controller;
 
+import com.viter.desafiocrudevsuperior.entite.Client;
+import com.viter.desafiocrudevsuperior.repositorie.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/client")
 public class ClientController {
 
+    @Autowired
+    private ClientRepository repository;
+
     @GetMapping
     public String test() {
-        return "Hello World!";
+        Optional<Client> result = repository.findById(1L);
+        Client client = result.get();
+        return client.getName();
     }
 }
---
+++ src/main/java/com/viter/desafiocrudevsuperior/repositorie/ClientRepository.java
@@ -0,0 +1,9 @@
+package com.viter.desafiocrudevsuperior.repositorie;
+
+import com.viter.desafiocrudevsuperior.entite.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
+
+
+}
